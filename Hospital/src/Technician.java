87bd7d77218d3853576a6baa8725ee3ffcfa7293
Marek Gargas <250497@wp.pl>
2019-04-11 00:31:09 +0200

    code refactor

--- Hospital/src/Technician.java
+++ Hospital/src/Technician.java
@@ -4,11 +4,13 @@ import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Technician extends HospitalWorker {
-    private HashMap<String, String> specialisationsQueues = new HashMap<>();
-
+    private List<String> specialisationsList = new LinkedList<>();
+    private Consumer consumer;
     private Technician() throws Exception {
         super();
 
@@ -16,9 +18,10 @@ public class Technician extends HospitalWorker {
 
     public static void main(String[] args) throws Exception {
         Technician technician = new Technician();
-        technician.setSpecialisationsQueues(args);
+        technician.setSpecialisationsList(Arrays.asList(args));
+        technician.createQueues();
         technician.bindQueues();
-        Consumer consumer = new DefaultConsumer(technician.channel) {
+        technician.consumer = new DefaultConsumer(technician.channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope,
                                        AMQP.BasicProperties properties, byte[] body) throws IOException {
@@ -34,27 +37,38 @@ public class Technician extends HospitalWorker {
 
             }
         };
-        technician.specialisationsQueues.values().forEach(queue -> {
+        technician.setUpConsuming();
+    }
+
+    private void setSpecialisationsList(List<String> specialisationsList) {
+        this.specialisationsList = specialisationsList;
+    }
+
+    private void createQueues() {
+        this.specialisationsList.forEach(specialisation -> {
             try {
-                technician.channel.basicConsume(queue, AUTO_ACK, consumer);
+                this.channel.queueDeclare(specialisation, true, false, false, null);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+    private void bindQueues() {
+        this.specialisationsList.forEach(specialisation -> {
+            try {
+                this.channel.queueBind(specialisation, HOSPITAL_EXCHANGE, specialisation);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
     }
 
-    private void setSpecialisationsQueues(String[] specialisations) throws Exception {
-        for (String specialisation : specialisations) {
-            //String specialisationQueue = this.channel.queueDeclare().getQueue();
-            this.channel.queueDeclare(specialisation, true, false, false, null);
-            this.specialisationsQueues.put(specialisation, specialisation);
-        }
-    }
-
-    private void bindQueues() throws IOException {
-        this.specialisationsQueues.forEach((specialisation, queue) -> {
+    private void setUpConsuming() {
+        this.specialisationsList.forEach(queue -> {
             try {
-                this.channel.queueBind(queue, HOSPITAL_EXCHANGE, specialisation);
+                this.channel.basicConsume(queue, AUTO_ACK, this.consumer);
             } catch (IOException e) {
                 e.printStackTrace();
             }
